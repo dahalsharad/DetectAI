8a6fabb37b92411723c17d501bdc6865f5e8cc43
dahalsharad <dahalsharad57@gmail.com>
2024-01-09 11:13:25 +0545

    added dataset

--- app.py
+++ app.py
@@ -30,6 +30,8 @@ def predict(random_text):
         # Transform the new text using the loaded TF-IDF vectorizer
         random_text_tfidf = tfidf_vectorizer.transform([random_text])
 
+        print(random_text_tfidf)
+
         prediction = [0, 0, 0, 0]
 
         # Make predictions using the loaded classifier
