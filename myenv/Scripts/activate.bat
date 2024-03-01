28c2235afbd44a987bf910535ef6ba781d3c5b97
Siddhant <076bct083@ioepc.edu.np>
2024-03-01 09:01:26 +0545

    updated requirements.txt

--- myenv/Scripts/activate.bat
+++ myenv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\Major Project\DetectAI\myenv
+set VIRTUAL_ENV=C:\Users\Siddhant\Desktop\proj\DetectAI\myenv
 
 if not defined PROMPT set PROMPT=$P$G
 
